a4a2379addf456740c4a8a9d4f51e0e481bb7cd4
Your Name <you@example.com>
2026-01-09 14:38:10 +0500

    start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd /home/ubuntu/microservices/service1
-sudo it pull
-sudo docker build -t service1:latest .
+#cd /home/ubuntu/microservices/service1
+#sudo it pull
+#sudo docker build -t service1:latest .
 
-sudo docker run -d \
-  --name service1 \
-  -p 3000:3001 \
-  --restart always \
-  service1:latest
+#sudo docker run -d \
+#  --name service1 \
+#  -p 3000:3001 \
+#  --restart always \
+#  service1:latest
+#
+sudo docker compose pull && sudo docker compose up -d && sudo docker image prune -f
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-sudo docker stop service1 || true
-sudo docker rm service1 || true
-sudo docker rmi service1:latest || true
+##!/bin/bash
+#sudo docker stop service1 || true
+#sudo docker rm service1 || true
+#sudo docker rmi service1:latest || true
